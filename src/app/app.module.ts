34292b48e725914cb819585d7b32e22f777cdad9
NOMBRE <vianey.hernandez@kumma.com.mx>
2022-01-13 12:12:00 -0600

    Website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';  
+import { FooterComponent } from './sharedpages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Nav2Component } from './sharedpages/nav2/nav2.component'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,   
+    FooterComponent,
+    HomeComponent,
+    Nav2Component 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
